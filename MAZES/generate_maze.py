a31dc5f661926c7fa73a82868fee07133e48b099
Ricardo Morato <rmr@cin.ufpe.br>
2021-10-18 20:42:28 -0300

    feat[maze]: create new method to list all successors for a MazeLocation

--- MAZES/generate_maze.py
+++ MAZES/generate_maze.py
@@ -49,5 +49,22 @@ class Maze:
     def goal_test(self, ml: MazeLocation) -> bool:
         return ml == self.goal
 
+    def successors(self, ml: MazeLocation) -> List[MazeLocation]:
+        locations: List[MazeLocation] = []
+
+        if ml.row + 1 < self._rows and self._grid[ml.row + 1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row + 1, ml.column))
+
+        if ml.row - 1 >= 0 and self._grid[ml.row - 1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row - 1, ml.column))
+
+        if ml.column + 1 < self._columns and self._grid[ml.row][ml.column + 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column + 1))
+
+        if ml.column - 1 >= 0 and self._grid[ml.row][ml.column - 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column - 1))
+
+        return locations
+
 maze: Maze = Maze()
 print(maze)
